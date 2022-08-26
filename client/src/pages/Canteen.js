1709e972ce2c9c6c5b564a5be80f65de9fe18929
2802AdityA <adityasingla.2802@gmail.com>
2022-08-27 04:00:48 +0530

    Different interface for student and Canteen

--- client/src/pages/Canteen.js
+++ client/src/pages/Canteen.js
@@ -1,7 +1,11 @@
-function Canteen(){
-    <div>
-        <h1>Hello ! </h1>
-    </div>
+import React from "react";
+
+function Canteen() {
+	return (
+		<div>
+			<h1>Hello ! </h1>
+		</div>
+	);
 }
 
 export default Canteen;
