89dfac07dce11289b8e8efd30b1738cc82c9088a
2802AdityA <adityasingla.2802@gmail.com>
2022-08-29 14:57:19 +0530

    Add menu table

--- client/src/pages/Canteen.js
+++ client/src/pages/Canteen.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
 
+const GET_MENU = gql`
+	query GetMenu {
+		menu {
+			id
+			name
+			price
+			quantity
+		}
+	}
+`;
 const Canteen = () => {
+	const { loading, error, data } = useQuery(GET_MENU);
+	const menuList = data?.menu;
+
+	// FOR CHECKING
+	console.log(loading);
+	console.log(error);
+	console.log(data);
+
 	return (
 		<div>
-			<h1>Hello ! </h1>
+			<table>
+				<tr>
+					<th>S No.</th>
+					<th>Name</th>
+					<th>Price</th>
+					<th>Quantity</th>
+				</tr>
+				{!error
+					? menuList?.map((itemDetails) => {
+							return (
+								<tr>
+									<th>{itemDetails.id}</th>
+									<th>{itemDetails.name}</th>
+									<th>{itemDetails.price}</th>
+									<th>{itemDetails.quantity}</th>
+								</tr>
+							);
+					  })
+					: "Something went wrong, Check back after sometime "}
+			</table>
 		</div>
 	);
 };
