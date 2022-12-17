a126513aff0139056f834faa052ce901e94d4917
Mahikagarg09 <mahika_garg@yahoo.com>
2022-12-17 21:28:21 +0530

    added e-mail

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -10,7 +10,8 @@ const Dashboard = () => {
 		setEmail(user.email); // eslint-disable-next-line
 	}, []);
 	// const canteenEmail = "muskangarg02270@gmail.com";
-	const canteenEmail = "adityasingla.2802@gmail.com";
+	// const canteenEmail = "adityasingla.2802@gmail.com";
+	const canteenEmail="mahikagarg23@gmail.com";
 	return email === canteenEmail ? <Student /> : <Canteen />;
 };
 
