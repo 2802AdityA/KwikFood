50cedb65a41cad8ddfbe128728579615f4cc7d47
Mahikagarg09 <mahika_garg@yahoo.com>
2022-12-18 16:05:31 +0530

    added component canteen-card

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Canteen from "./Canteen";
 import Student from "./Student";
+import Canteencards from "./Canteencards";
 
 const Dashboard = () => {
 	const [email, setEmail] = useState("");
@@ -10,9 +11,9 @@ const Dashboard = () => {
 		setEmail(user.email); // eslint-disable-next-line
 	}, []);
 	// const canteenEmail = "muskangarg02270@gmail.com";
-	// const canteenEmail = "adityasingla.2802@gmail.com";
-	const canteenEmail = "mahikagarg23@gmail.com";
-	return email === canteenEmail ? <Canteen /> : <Student />;
+	const canteenEmail = "adityasingla.2802@gmail.com";
+	// const canteenEmail = "mahikagarg23@gmail.com";
+	return email === canteenEmail ? <Canteen /> : <Canteencards/>;
 };
 
 export default Dashboard;
