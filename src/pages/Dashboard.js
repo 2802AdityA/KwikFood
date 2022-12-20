23a5a77c01abc424fadafc9e295357d130a0153a
AdityA <84802479+2802AdityA@users.noreply.github.com>
2022-12-21 03:20:24 +0530

    Remove different signin and add canteen component for canteen email

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Canteen from "./Canteen";
 import Student from "./Student";
-import { gql, useQuery} from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 
 const check_canteen_user = gql`
 query canteenUser($email: String) {
@@ -20,16 +20,17 @@ const Dashboard = () => {
 		setEmail(user.email); // eslint-disable-next-line
 	}, []);
 
-	const {data, loading, error} = useQuery(check_canteen_user, {variables: {email}});
-	
+	const { data, loading, error } = useQuery(check_canteen_user, { variables: { email } });
+
 	if (loading) return "Loading..";
 	if (error) return `Error! ${error.message}`
-	else{
-		if(data.canteen_email.length==0){
-			return <Student />
+	else {
+		console.log(data);
+		if (data.canteen_email[0].owner_email == email) {
+			return <Canteen />
 		}
-		else{
-			return <Canteen/>
+		else {
+			return <Student />
 		}
 	}
 };
