1c17d82f5df91dc4b35b2b67d76872770ef6603a
muskan garg <muskangarg02270@gmail.com>
2022-12-20 21:27:17 +0530

    sign-in query

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,24 +1,37 @@
 import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Canteen from "./Canteen";
-
 import Student from "./Student";
-import Canteencurrentorders from "./Canteencurrentorders";
+import { gql, useQuery} from "@apollo/client";
 
+const check_canteen_user = gql`
+query canteenUser($email: String) {
+	canteen_email(where: {owner_email: {_eq: $email}}){
+	  owner_email
+	}
+  }  
+`
 
 const Dashboard = () => {
+
 	const [email, setEmail] = useState("");
 	const { user } = useOutletContext();
 	useEffect(() => {
 		setEmail(user.email); // eslint-disable-next-line
 	}, []);
-	// const canteenEmail = "muskangarg02270@gmail.com";
 
-	// const canteenEmail = "adityasingla.2802@gmail.com";
-	const canteenEmail = "mahikagarg23@gmail.com";
-	return email === canteenEmail ? <Canteen /> : <Student />
-	// return email === canteenEmail ? <Canteencurrentorders /> : <Student />;
-
+	const {data, loading, error} = useQuery(check_canteen_user, {variables: {email}});
+	
+	if (loading) return "Loading..";
+	if (error) return `Error! ${error.message}`
+	else{
+		if(data.canteen_email.length==0){
+			return <Student />
+		}
+		else{
+			return <Canteen/>
+		}
+	}
 };
 
 export default Dashboard;
