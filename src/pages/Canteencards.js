cd6986b162cf7996b038c0e5c5c31838f927dc57
AdityA <84802479+2802AdityA@users.noreply.github.com>
2023-03-22 06:26:15 +0530

    Navigation to Student

--- src/pages/Canteencards.js
+++ src/pages/Canteencards.js
@@ -1,8 +1,9 @@
-import { gql, useQuery } from '@apollo/client';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
 import cardimage from "../assets/card-image.jpg"
 import "../styles/Student/Canteencard.css"
-import Student from './Student';
 
 const GET_CANTEEN = gql`query GetCanteen {
     canteen_email {
@@ -10,22 +11,24 @@ const GET_CANTEEN = gql`query GetCanteen {
       owner_email
     }
   }`
+
 export default function Canteencards() {
+    const navigate = useNavigate();
     const { data, loading, error } = useQuery(GET_CANTEEN);
+
     if (loading) return "Loading..";
     if (error) return `Error! ${error.message}`
+
     const canteenEmail = data?.canteen_email;
 
-    const cardChoose = (email) => {
-        console.log(email);
-        // Return Student page with canteen email as a prop
-        return <Student email={email} />
+    const cardChoose = (email, name) => {
+        navigate(`/canteens/${name}`, { state: { email } });
     }
+
     return (
         <div className='row'>
-
             {canteenEmail.map((canteen) => (
-                <div className="col-lg-6 col-md-6" onClick={() => { cardChoose(canteen.owner_email) }}>
+                <div className="col-lg-6 col-md-6" onClick={() => { cardChoose(canteen.owner_email, canteen.canteen_name) }}>
                     <div className="card bg-transparent">
                         <img src={cardimage} className="card-img" alt="" />
                         <div className="card-img-overlay">
@@ -33,8 +36,7 @@ export default function Canteencards() {
                         </div>
                     </div>
                 </div>
-            ))
-            }
+            ))}
         </div>
     )
 }
